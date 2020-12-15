2505767b1446dd569f48d09256fdc004e5ffd6fb
under305 <underjoel30@gmail.com>
2020-12-15 17:43:11 -0600

    check

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-	
+
+	$("#solicitarCreditoPc").click(function(event) {
+		//openAlarg();
+		//localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZDkzZGQ3YTk1NGMzMGVkNWRjNjBkNyIsImlhdCI6MTYwODA3MjY2M30.waCYt33YBVvWqDuqJmnf_0jGb1ijgafeJbg0_Kh_dK0");
+		//console.log(localStorage.getItem("token"));
+		$.ajax({
+			url: 'http://localhost:4000/users/hasFiscal',
+			headers: {
+				"Authorization": "Bearer "+localStorage.getItem('token')
+			}
+		})
+		.done(function(data) {
+			if(data){
+				console.log(data);
+			}else{
+				console.log(data);
+				openAlarg();
+			}
+		})
+		.fail(function() {
+			console.log("error");
+		});
+		
+	});
 });
