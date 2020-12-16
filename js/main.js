3589811abfb5339b97d9110c12bf1912c7ac0ad7
under305 <underjoel30@gmail.com>
2020-12-15 21:28:23 -0600

    Nuevo a main.js

--- js/main.js
+++ js/main.js
@@ -24,7 +24,64 @@ $(document).ready(function() {
 	});
 
 	$("#sendFiscalData").click(function(event) {
-		$.ajax({
+		var rfc = $("#rfc").val();
+		if(
+			rfc = $("#rfc").val().length 			!= 12 ||
+			$("#curp").val().length 				== 0 ||
+			$("#address").val().length 				== 0 ||
+			$("#cp").val().length 					== 0 ||
+			$("#social").val().length 				== 0 ||
+			$("#email").val().length 				== 0 ||
+			$("#phone").val().length				== 0 ||
+			$("#occupation").val().length			== 0 ||
+			$("#monthlyIncome").val()				== 0 ||
+			$("#workplace").val()					== 0 ||
+			$("#academic option:selected").text()	== 0 ||
+			$("#reference").val()					== 0 ||
+			$("#beneficiary").val()					== 0
+		){
+			alert("Algunos campos pueden estar incorrectos, por favor, verifíquelos");
+		}else{
+			$.ajax({
+				url: 'http://localhost:4000/fiscal/createFiscal',
+				type: 'POST',
+				headers: {
+					"Authorization": "Bearer "+localStorage.getItem('token')
+				},
+				data: {
+					'rfc': $("#rfc").val(),
+					'curp': $("#curp").val(),
+					'address': $("#address").val(),
+					'postal': $("#cp").val(),
+					'social': $("#social").val(),
+					'email': $("#email").val(),
+					'phone': $("#phone").val(),
+					'occupation': $("#occupation").val(),
+					'monthlyIncome': $("#monthlyIncome").val(),
+					'workplace': $("#workplace").val(),
+					'academic': $("#academic option:selected" ).text(),
+					'reference': $("#reference").val(),
+					'beneficiary': $("#beneficiary").val()
+				}
+				
+			})
+			.done(function(data) {
+				if(!data._id){
+					alert("Ha ocurrido un error, por favor, verifique los datos");	
+				}else{
+					closeReg(); 
+					closeIn2();
+					openAlarg();
+				}
+			})
+			.fail(function() {
+				alert("Ha ocurrido un error, por favor, verifique los datos");
+			})
+			.error(function() {
+				alert("Ha ocurrido un error, por favor, verifique los datos");
+			});
+		}
+		/*$.ajax({
 			url: 'http://localhost:4000/fiscal/createFiscal',
 			type: 'POST',
 			headers: {
@@ -47,13 +104,21 @@ $(document).ready(function() {
 			}
 			
 		})
-		.done(function() {
-			console.log("success");
+		.done(function(data) {
+			if(!data.id){
+				alert("Ha ocurrido un error, por favor, verifique los datos");	
+			}else{
+				console.log(data);
+			}
 		})
 		.fail(function() {
-			console.log("error");
+			alert("Ha ocurrido un error, por favor, verifique los datos");
+		})
+		.error(function() {
+			alert("Ha ocurrido un error, por favor, verifique los datos");
 		});
 		
+		*/
 	});
 
 });
